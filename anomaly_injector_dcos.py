61d1ecb1b4518a242e718d2d0936af67fcf56d6b
Brandonage <alvarobrandon@gmail.com>
2017-11-06 18:45:20 +0100

    refactored stress-ng. Added logs and more stress methods

--- anomaly_injector_dcos.py
+++ anomaly_injector_dcos.py
@@ -22,8 +22,124 @@ class AnomalyInjectorDCOS():
                hosts=nodes,
                connection_params=connection_params).run()
 
-    def stress_cpu(self,nodes,nstressors,time):
-        Remote(cmd="stress-ng --cpu {0} --timeout {1}s".format(nstressors,time),
+    def stress_ng(self,type,nstressors,timeout,nodes):
+        Remote(cmd="stress-ng --{0} {1} --timeout {2}s".format(type,nstressors,timeout),
                hosts=nodes,
                connection_params=self.connection_params
                ).start()
+
+    def stress_cpu(self,nodes,nstressors,timeout):
+        self.stress_ng(type='cpu',nstressors=nstressors,timeout=timeout,nodes=nodes)
+
+    def stress_disk(self,nodes,nstressors,timeout):
+        self.stress_ng(type='hdd', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_network(self,nodes,nstressors,timeout):
+        self.stress_ng(type='sock', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_lockbus(self,nodes,nstressors,timeout):
+        """
+        N stressors that rapidly lock and increment 64 bytes of randomly chosen memory from a 16MB mmap d region
+        (Intel x86 CPUs only). This will cause cacheline misses and stalling of CPUs.
+        :param nodes:
+        :param nstressors:
+        :param time:
+        """
+        self.stress_ng(type='lockbus', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_cache(self,nodes,nstressors,timeout):
+        """
+        start N stressors that perform random wide spread memory read and writes to thrash the CPU cache. The code does
+        not intelligently determine the CPU cache configuration and so it may be sub-optimal in producing hit-miss
+        read/write activity for some processors.
+        :param nodes:
+        :param nstressors:
+        :param time:
+        """
+        self.stress_ng(type='cache', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_big_heap(self,nodes,nstressors,timeout):
+        """
+        start N workers that grow their heaps by reallocating memory. If the out of memory killer (OOM) on Linux kills
+        the worker or the allocation fails then the allocating process starts all over again. Note that the OOM
+        adjustment for the worker is set so that the OOM killer will treat these workers as the first candidate
+        processes to kill.
+        :param nodes:
+        :param nstressors:
+        :param time:
+        """
+        self.stress_ng(type='bigheap', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_matrix(self,nodes,nstressors,timeout):
+        """
+        start N workers that perform various matrix operations on floating point values. Testing on 64 bit x86 hardware
+        shows that this provides a good mix of memory, cache and floating point operations and is an excellent way to
+        make a CPU run hot. By default, this will exercise all the matrix stress methods one by one.
+        :param nodes:
+        :param nstressors:
+        :param time:
+        """
+        self.stress_ng(type='matrix', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_stream_memory(self,nodes,nstressors,timeout):
+        """
+        start N workers exercising a memory bandwidth stressor loosely based on the STREAM "Sustainable Memory
+        Bandwidth in High Performance Computers" benchmarking tool by John D. McCalpin, Ph.D.
+        This stressor allocates buffers that are at least 4 times the size of the CPU L2 cache and continually
+        performs rounds of computations on large arrays of double precision floating point numbers
+        :param nodes:
+        :param nstressors:
+        :param time:
+        """
+        self.stress_ng(type='stream', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def stress_zlib(self,nodes,nstressors,timeout):
+        """
+        start N workers compressing and decompressing random data using zlib. Each worker has two processes, one that
+        compresses random data and pipes it to another process that decompresses the data.
+        This stressor exercises CPU, cache and memory.
+        :param nodes:
+        :param nstressors:
+        :param time:
+        """
+        self.stress_ng(type='zlib', nstressors=nstressors, timeout=timeout, nodes=nodes)
+
+    def limit_upload_bandwidth(self,nodes,delay='100ms',delay_jitter='1ms',bandwidth='100kbps',loss_percent='1%'):
+        Remote(cmd='sudo tc qdisc add dev eth0 root handle 1: htb default 10',
+               hosts=nodes,
+               connection_params=self.connection_params).run()
+        Remote(cmd='sudo tc class add dev eth0 parent 1: classid 1:10 htb rate {0} ceil {0}'.format(bandwidth),
+               hosts=nodes,
+               connection_params=self.connection_params).run()
+        Remote(cmd='sudo tc qdisc add dev eth0 parent 1:10 handle 10: netem delay {0} {1} 25% loss {2}'.format(delay,delay_jitter,loss_percent),
+               hosts=nodes,
+               connection_params=self.connection_params).run()
+        #sudo tc qdisc add dev eth0 root handle 1: htb default 10
+        #sudo tc class add dev eth0 parent 1: classid 1:10 htb rate 10kbps ceil 10kbps
+        #sudo tc qdisc add dev eth0 parent 1:10 handle 10: netem delay 100ms 0.1ms 25% loss 1%
+
+    def restore_upload_bandwidth(self,nodes):
+        Remote(cmd='sudo tc qdisc del dev eth0 root',
+               hosts=nodes,
+               connection_params=self.connection_params).run()
+
+
+    def reboot_machine(self,nodes):
+        Remote(cmd='sudo reboot',
+               hosts=nodes,
+               connection_params=self.connection_params).run()
+
+    def shutdown_machine(self,nodes):
+        Remote(cmd='sudo shutdown',
+               hosts=nodes,
+               connection_params=self.connection_params).run()
+
+    def fill_up_disk(self,nodes):
+        Remote(cmd="dd if=/dev/zero of=/tmp/tempFiller.deleteMe bs=1M count=1M",
+               host=nodes,
+               connection_params=self.connection_params).run()
+
+    def clear_up_disk(self,nodes):
+        Remote(cmd="rm /tmp/tempFiller.deleteMe",
+               host=nodes,
+               connection_params=self.connection_params).run()
--- microservices_rca.py
+++ microservices_rca.py
@@ -1,32 +1,58 @@
 import anomaly_injector_factory
 import workflow_injector_factory
+from random import shuffle
+import time
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 """
 This is the main class that is going to start and inject the failures on the DCOS cluster
 """
+
+
 class MicroServicesRCA():
-    def __init__(self,masters,public_agents,connection_params,orchestrator):
+    def __init__(self, masters, private_agents, public_agents, connection_params, orchestrator, experiment_log):
         # I dont know if we really need the master IP when we have the leader.mesos name to locate it
         # All and all there are three options: give to it one master, several masters, or None and just use
         # the leader.mesos name resolution with any of the slaves. I suppose this DNS also applies to K8
         """
-
-        :param master:
-        :param slaves:
-        :param connection_params:
+        :param masters: a list for the masters or endpoints that ingest the container workflows
+        :param private_agents: a list of the nodes that host the containers
+        :param public_agents: a list of public access nodes. Normally they will hold load balancers and similar
+        :param connection_params: a connection params dictionary for execo to connect to
+        :param orchestrator: the type of orchestrator we are going to use
+        :param experiment_log: a DF that has a log for all of the container workflows launched and anomalies injected
         """
         self.masters = masters
-        # We need all the slaves in order to do things like inject delay between two random nodes
+        # We choose to differentiate between private_agents and public_agents because they can have different purposes
+        self.private_agents = private_agents
         self.public_agents = public_agents
+        # We need all the slaves in order to do things like inject delay between two random nodes
+        self.nodes = masters.union(public_agents.union(private_agents))
         # This is going to be the anomaly injector. Is going to have as a parameter some connection parameters for execo
         # We are going to have only one, but we could have more than one if we wanted to inject failures at different
         # levels. e.g. an anomaly injector at physical level, another at VM level, another on some other cluster with different
         # credentials and so on... for the sake of simplicity only one here
         # we also pass as a parameter the nodes of the cluster since we need to install some software on them to
-        # generate these anomalies
-        self.anomaly_injector = anomaly_injector_factory.get_anomaly_injector(orchestrator, masters.union(public_agents), connection_params)
+        # generate these anomalies. HOWEVER we are only using them for the anomaly injector at the moment
+        self.anomaly_injector = anomaly_injector_factory.get_anomaly_injector(orchestrator,
+                                                                              self.nodes,
+                                                                              connection_params)
         # same as the anomaly injector. But this one is going to inject workflows
-        self.workflow_injector = workflow_injector_factory.get_workflow_injector(orchestrator,masters.union(public_agents),connection_params)
+        self.workflow_injector = workflow_injector_factory.get_workflow_injector(orchestrator,
+                                                                                 self.nodes,
+                                                                                 connection_params)
+        # we are going to write into this log every operation done by the testbed in order to do a post analysis of the experiment
+        self.experiment_log = experiment_log
+
+    def write_to_experiment_log(self, index, type, name, nodes, start, end, info):
+        self.experiment_log.loc[index] = {
+            "type": type,
+            "name": name,
+            "nodes": nodes,
+            "date_start": start,
+            "date_end": end,
+            "aditional_info": info
+        }
 
     # From here we are going to include the scenarios that inject both workloads and anomalies. We decided to keep
     # this logic here in the MicroServicesRCA object instead of calling directly the injectors functions.
@@ -34,8 +60,137 @@ class MicroServicesRCA():
     # injectors to create some scenarios. Anyways we also have the option of using the injectors but we will try to
     # keep as much logic here as possible
 
-    def kafka_producer_consumer_scenario(self,nbrokers,nconsumers,nproducers):
-        self.workflow_injector.kafka_producers_and_consumers(self.masters,nbrokers,nconsumers,nproducers)
+    def kafka_producer_consumer_scenario(self, nbrokers, nconsumers, nproducers):
+        self.write_to_experiment_log(current_milli_time(), "workflow", "kafka_producer_consumer", "Scheduled by DCOS",
+                                     int(time.time()),
+                                     "unknown",
+                                     "Brokers: {0} Producers: {1} Consumers: {2}".format(nbrokers, nproducers, nconsumers))
+        self.workflow_injector.kafka_producers_and_consumers(self.masters, nbrokers, nconsumers, nproducers)
 
-    def stress_cpu_nodes(self,nodes, nstressors, time):
-        self.anomaly_injector.stress_cpu(nodes,nstressors,time)
+    # This are the stress methods. We are going to write in the experiment log the type of anomaly
+    # and the time when the mentioned anomaly started.
+
+    def stress_cpu_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_cpu_nodes", list(nodes), int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_cpu(nodes, nstressors, timeout)
+
+    def stress_cpu_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_cpu_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_disk_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_disk_nodes", list(nodes), int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_disk(nodes, nstressors, timeout)
+
+    def stress_disk_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_disk_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_network_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_network_nodes", list(nodes), int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_network(nodes, nstressors, timeout)
+
+    def stress_network_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_network_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_lockbus_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_lockbus_nodes", list(nodes), int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_lockbus(nodes, nstressors, timeout)
+
+    def stress_lockbus_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_lockbus_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_cache_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_cache_nodes", list(nodes), int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_cache(nodes, nstressors, timeout)
+
+    def stress_cache_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_cache_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_big_heap_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_big_heap_nodes", nodes, int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_big_heap(nodes, nstressors, timeout)
+
+    def stress_big_heap_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_big_heap_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_matrix_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_matrix_nodes", nodes, int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_matrix(nodes, nstressors, timeout)
+
+    def stress_matrix_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_matrix_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_stream_memory_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_stream_memory_nodes", nodes, int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_stream_memory(nodes, nstressors, timeout)
+
+    def stress_stream_memory_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_stream_memory_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def stress_zlib_nodes(self, nodes, nstressors, timeout):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "stress_zlib_nodes", nodes, int(time.time()),
+                                     int(time.time()) + timeout, "Stressors: {0}".format(nstressors))
+        self.anomaly_injector.stress_zlib(nodes, nstressors, timeout)
+
+    def stress_zlib_nodes_random(self, nnodes, nstressors, timeout):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.stress_zlib_nodes(set(list_nodes[0:nnodes]), nstressors, timeout)
+
+    def limit_upload_bandwidth_nodes(self,nodes,delay='100ms',delay_jitter='1ms',bandwidth='100kbps',loss_percent='1%'):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "limit_upload_bandwidth_nodes", nodes, int(time.time()),
+                                     0, "delay:{0},delay_jitter:{1},bandwidth:{2},loss_percent:{3}".format(delay,delay_jitter,bandwidth,loss_percent))
+        self.anomaly_injector.limit_upload_bandwidth(nodes,delay,delay_jitter,bandwidth,loss_percent)
+
+    def limit_upload_bandwidth_nodes_random(self,nnodes,delay='100ms',delay_jitter='1ms',bandwidth='100kbps',loss_percent='1%'):
+        list_nodes = list(self.nodes)
+        shuffle(list_nodes)
+        self.limit_upload_bandwidth_nodes(set(list_nodes[0:nnodes]),delay,delay_jitter,bandwidth,loss_percent)
+
+    def restore_upload_bandwidth_nodes(self,nodes):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "restore_upload_bandwidth", nodes, int(time.time()),
+                                     0, "No info")
+        self.anomaly_injector.restore_upload_bandwidth(nodes)
+
+    def reboot_machine_nodes(self,nodes):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "reboot_machine_nodes", nodes, int(time.time()),
+                                     0, "No info")
+        self.anomaly_injector.reboot_machine(nodes)
+
+    def shutdown_machine_nodes(self,nodes):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "shutdown_machine_nodes", nodes, int(time.time()),
+                                     0, "No info")
+        self.anomaly_injector.shutdown_machine(nodes)
+
+    def fill_up_disk_nodes(self,nodes):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "fill_up_disk_nodes", nodes, int(time.time()),
+                                     0, "No info")
+        self.anomaly_injector.fill_up_disk(nodes)
+
+    def clear_up_disk_nodes(self,nodes):
+        self.write_to_experiment_log(current_milli_time(), "anomaly", "clear_up_disk_nodes", nodes, int(time.time()),
+                                     0, "No info")
+        self.anomaly_injector.clear_up_disk(nodes)
